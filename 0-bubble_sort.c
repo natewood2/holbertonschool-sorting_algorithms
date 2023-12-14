3083e07e832db052bfa5a633ca5d1d5f5baad241
natewood2 <nathanwood.sfg@gmail.com>
2023-12-14 09:39:26 -0600

    adding all the work I did last night

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using the Bubble Sort
+ * @array: A pointer to the first element of the array of integers to be sorted.
+ * @size: The number of elements in the array.
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+    int temp;
+
+    if (array == NULL || size < 2)
+        return;
+
+    for (i = 0; i < size - 1; i++)
+    {
+
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                print_array(array, size);
+            }
+        }
+    }
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,75 @@
+#include "sort.h"
+
+/**
+ * 
+ * 
+ * 
+ * 
+*/
+void swap_node(listint_t **list, listint_t *node, listint_t *node_two)
+{
+    listint_t *temp;
+
+    if (node->next == node_two) 
+    {
+        temp = node_two->next;
+        node_two->next = node;
+        node->next = temp;
+
+        temp = node->prev;
+        node->prev = node_two;
+        node_two->prev = temp;
+    } 
+    else 
+    {
+        node->next = node_two->next;
+        node_two->next = node;
+
+        temp = node->prev;
+        node->prev = node_two->prev;
+        node_two->prev = temp;
+    }
+    if (node->next != NULL) 
+    {
+        node->next->prev = node;
+    }
+    if (node_two->prev != NULL) 
+    {
+        node_two->prev->next = node_two;
+    }
+    if (node_two->prev == NULL) 
+    {
+        *list = node_two;
+    }
+}
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+*/
+void insertion_sort_list(listint_t **list)
+{
+    if (list == NULL || *list == NULL)
+    {
+        return;
+    }
+
+    listint_t *current, *insert;
+
+    current = (*list)->next;
+
+    while (current != NULL)
+    {
+        insert = current->prev;
+        while (insert != NULL && current->n < insert->n)
+        {
+            swap_node(list, insert, current);
+            print_list(*list);
+            insert = current->prev;
+        }
+        current = current->next;
+    }
+}
